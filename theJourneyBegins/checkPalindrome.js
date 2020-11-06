09b87adbb6b9cfb514d8bc3db505a3f9d96cc329
Maritza Stabile <maritza723@yahoo.com>
2020-11-05 16:45:41 -0800

    algorithm practice

--- theJourneyBegins/checkPalindrome.js
+++ theJourneyBegins/checkPalindrome.js
@@ -21,9 +21,19 @@
  * 
  * true if inputString is a palindrome, false otherwise. */
 function checkPalindrome(inputString) {
-
+    let len = inputString.length - 1;
+    let j = len;
+    console.log(inputString[j]);
+    for (let i = 0; i < len/2; i++, j--){
+        console.log(inputString[i]);
+        // 
+        if(inputString[i] != inputString[j]){
+            return false;
+        } 
+    }
+    return true;
 }
 
- // console.log(checkPalindrome(aabaa) + " == true");
-// console.log();
-// console.log(checkPalindrome(abac) + " == false");
+console.log(checkPalindrome("aabaa") + " == true");
+console.log();
+console.log(checkPalindrome("abac") + " == false");
