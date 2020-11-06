1c71754f9fa2ef61f4385b9712bb1d42ac63b99e
Maritza Stabile <maritza723@yahoo.com>
2020-11-05 16:52:11 -0800

    algorithm practice

---
+++ edgeOfTheOcean/adjacentElementsProduct.js
@@ -0,0 +1,31 @@
+/**Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+ * Example
+ * 
+ * For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+ * adjacentElementsProduct(inputArray) = 21.
+ * 
+ * 7 and 3 produce the largest product.
+ * 
+ * Input/Output
+ * 
+ * [execution time limit] 4 seconds (js)
+ * 
+ * [input] array.integer inputArray
+ * 
+ * An array of integers containing at least two elements.
+ * 
+ * Guaranteed constraints:
+ * 2 ≤ inputArray.length ≤ 10,
+ * -1000 ≤ inputArray[i] ≤ 1000.
+ * 
+ * [output] integer
+ * 
+ * The largest product of adjacent elements. */
+
+function adjacentElementsProduct(inputArray) {
+
+};
+
+// console.log(checkPalindrome([3, 6, -2, -5, 7, 3]) + " == 21");
+// console.log();
+// console.log(checkPalindrome([5, 1, 2, 3, 1, 4]) + " == 6");
